0c5fcbff09cd8e8feab9fb05fcca313759d27537
Conrad Buck <conartist6@gmail.com>
2020-07-31 12:56:14 -0700

    keys(), values(), entries()

--- lib/queue.d.ts
+++ lib/queue.d.ts
@@ -1,9 +1,26 @@
+/**
+ * The values which has been in the queue longest is its head.
+ * The most recent addition is the tail.
+ */
 declare class Queue<T> {
   /**
-   * An optional iterable of `values` to be pushed into the queue in sequence. If `null` or `undefined` are passed the queue will have no initial values.
+   * Returns true if `inst` is a Queue.
+   * This does not necessarily imply instanceof, but the check
+   * is safe across frame boundaries, as it is done by looking for
+   * Queue[Symbol.for('@iter-tools/queue')]
+   */
+  static isQueue(inst): boolean;
+
+  /**
+   * An optional iterable of `values` to be pushed into the queue
+   * in sequence. If `null` or `undefined` are passed the queue will
+   * have no initial values.
    */
   constructor(values?: Iterable<T>);
 
+  /**
+   * The number of values in the queue
+   */
   size: number;
 
   /**
@@ -22,10 +39,29 @@ declare class Queue<T> {
   push(value: T);
 
   /**
-   * Calls `cb(value)` for each value in the queue
+   * Calls `cb(value)` for each value in the queue.
    */
   forEach(cb: (T) => any);
 
+  /**
+   * Yields sequential 0-based indexes, one for each queued value.
+   * The index 0 indicates the head.
+   */
+  keys(): IterableIterator<number>;
+
+  /**
+   * Yields the queued values from head to tail.
+   */
+  values(): IterableIterator<T>;
+
+  /**
+   * Yields `[index, value]` tuples for each value in the queue.
+   */
+  entries(): IterableIterator<[number, T]>;
+
+  /**
+   * The default iterator. Equivalent to `values()`.
+   */
   [Symbol.iterator](): IterableIterator<T>;
 }
 
