4b307244c5a7d52eb818109a81182058e9bb3978
Conrad Buck <conartist6@gmail.com>
2020-07-31 23:27:03 -0700

    clear

--- lib/queue.d.ts
+++ lib/queue.d.ts
@@ -11,57 +11,26 @@ declare class Queue<T> {
    */
   static isQueue(inst): boolean;
 
-  /**
-   * An optional iterable of `values` to be pushed into the queue
-   * in sequence. If `null` or `undefined` are passed the queue will
-   * have no initial values.
-   */
   constructor(values?: Iterable<T> | null);
 
-  /**
-   * The number of values in the queue
-   */
   readonly size: number;
 
-  /**
-   * Returns the value at the head of the queue.
-   */
+  clear(): void;
+
   peek(): T | undefined;
 
-  /**
-   * Removes the value at the head of the queue and returns it.
-   */
   shift(): T | undefined;
 
-  /**
-   * Adds `value` at the tail of the queue.
-   */
   push(value: T): void;
 
-  /**
-   * Calls `cb(value, index, queue)` for each value in the queue.
-   */
   forEach(cb: (value: T, index: number, queue: Queue) => any): void;
 
-  /**
-   * Yields sequential 0-based indexes, one for each queued value.
-   * The index 0 indicates the head.
-   */
   keys(): IterableIterator<number>;
 
-  /**
-   * Yields the queued values from head to tail.
-   */
   values(): IterableIterator<T>;
 
-  /**
-   * Yields an `[index,value]` tuple for each value in the list.
-   */
   entries(): IterableIterator<[number, T]>;
 
-  /**
-   * The default iterator. Equivalent to `values()`.
-   */
   [Symbol.iterator](): IterableIterator<T>;
 }
 
