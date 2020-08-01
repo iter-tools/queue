ce7981b7f7b2cd45d474a72245dc9e74610b38c5
Conrad Buck <conartist6@gmail.com>
2020-07-31 22:38:47 -0700

    Doc fixup

--- lib/queue.d.ts
+++ lib/queue.d.ts
@@ -4,10 +4,10 @@
  */
 declare class Queue<T> {
   /**
-   * Returns true if `inst` is a Queue.
+   * Returns true if `inst` is a `Queue`.
    * This does not necessarily imply instanceof, but the check
    * is safe across frame boundaries, as it is done by looking for
-   * Queue[Symbol.for('@iter-tools/queue')]
+   * inst[Symbol.for('@iter-tools/queue')]
    */
   static isQueue(inst): boolean;
 
@@ -16,12 +16,12 @@ declare class Queue<T> {
    * in sequence. If `null` or `undefined` are passed the queue will
    * have no initial values.
    */
-  constructor(values?: Iterable<T>);
+  constructor(values?: Iterable<T> | null);
 
   /**
    * The number of values in the queue
    */
-  size: number;
+  readonly size: number;
 
   /**
    * Returns the value at the head of the queue.
@@ -36,12 +36,12 @@ declare class Queue<T> {
   /**
    * Adds `value` at the tail of the queue.
    */
-  push(value: T);
+  push(value: T): void;
 
   /**
    * Calls `cb(value, index, queue)` for each value in the queue.
    */
-  forEach(cb: (value: T, index: number, queue: Queue) => any);
+  forEach(cb: (value: T, index: number, queue: Queue) => any): void;
 
   /**
    * Yields sequential 0-based indexes, one for each queued value.
@@ -55,7 +55,7 @@ declare class Queue<T> {
   values(): IterableIterator<T>;
 
   /**
-   * Yields `[index, value]` tuples for each value in the queue.
+   * Yields an `[index,value]` tuple for each value in the list.
    */
   entries(): IterableIterator<[number, T]>;
 
